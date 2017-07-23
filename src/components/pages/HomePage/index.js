fd83f15c7f0cc977556537e7fde5c895c4e70cbe
mauget <mauget@mindspring.com>
2017-07-23 10:05:40 -0400

    Bootstrap col_sm

--- src/components/pages/HomePage/index.js
+++ src/components/pages/HomePage/index.js
@@ -5,113 +5,86 @@ const HomePage = () => {
   return <div className="container" style={{border: "solid red 1px"}}>
 
     <div className="row">
-      <div className="col-md-2"></div>
-      <div className="col-md-8">
-        <h3 style={{width: "100%", "text-align": "center"}}>Section 1</h3>
+      <div className="col-sm-12">
+        <h5 style={{width: "100%", "text-align": "center"}}>Section 1</h5>
       </div>
-      <div className="col-md-2"></div>
     </div>
 
     <div className="row">
-      <div className="col-md-2"></div>
-      <div className="col-md-8">
+      <div className="col-sm-12">
         <div id="mountPoint"></div>
       </div>
-      <div className="col-md-2"></div>
     </div>
 
     <div className="row">
-      <div className="col-md-2"></div>
-      <div className="col-md-8">
+      <div className="col-sm-12">
         <h4 style={{width: "100%"}}>Name</h4>
       </div>
-      <div className="col-md-2"></div>
     </div>
 
     <div className="row">
-      <div className="col-md-2"></div>
-      <div className="col-md-8">
+      <div className="col-sm-12">
         <input style={{width: "100%"}} value="Elwood P. Suggins"/>
       </div>
-      <div className="col-md-2"></div>
     </div>
 
     <div className="row">
-      <div className="col-md-2"></div>
-      <div className="col-md-8">
+      <div className="col-sm-12">
         <h4 style={{width: "100%"}}>City</h4>
       </div>
-      <div className="col-md-2"></div>
     </div>
 
     <div className="row">
-      <div className="col-md-2"></div>
-      <div className="col-md-8">
+      <div className="col-sm-12">
         <input style={{width: "100%"}} value="Dogpatch"/>
       </div>
-      <div className="col-md-2"></div>
     </div>
 
     <div className="row">
-      <div className="col-md-2"></div>
-      <div className="col-md-8">
-        <h3 style={{width: "100%", "text-align": "center"}}>Section 2</h3>
+      <div className="col-sm-12">
+        <h5 style={{width: "100%", "text-align": "center"}}>Section 2</h5>
       </div>
-      <div className="col-md-2"></div>
     </div>
 
     <div className="row">
-      <div className="col-md-2"></div>
-      <div className="col-md-8">
+      <div className="col-sm-12">
         <h4 style={{width: "100%"}}>Hobby</h4>
       </div>
-      <div className="col-md-2"></div>
     </div>
 
     <div className="row">
-      <div className="col-md-2"></div>
-      <div className="col-md-8">
+      <div className="col-sm-12">
         <input style={{width: "100%"}} value="Coffee tasting"/>
       </div>
-      <div className="col-md-2"></div>
     </div>
 
     <div className="row">
-      <div className="col-md-2"></div>
-      <div className="col-md-8">
+      <div className="col-sm-12">
         <h4 style={{width: "100%"}}>Occupation</h4>
       </div>
-      <div className="col-md-2"></div>
     </div>
 
     <div className="row">
-      <div className="col-md-2"></div>
-      <div className="col-md-8">
+      <div className="col-sm-12">
         <input style={{width: "100%"}} value="Meeting attendee"/>
       </div>
-      <div className="col-md-2"></div>
     </div>
 
     <div className="row">
-      <div className="col-md-2"></div>
-      <div className="col-md-8">
-        <h3 style={{width: "100%", "text-align": "center"}}>Section 3</h3>
+      <div className="col-sm-12">
+        <h5 style={{width: "100%", "text-align": "center"}}>Section 3</h5>
       </div>
-      <div className="col-md-2"></div>
     </div>
 
 
     <div className="row">
-      <div className="col-md-2"></div>
-      <div className="col-md-8">
+      <div className="col-sm-12">
         <h4 style={{width: "100%"}}>Notes</h4>
       </div>
-      <div className="col-md-2"></div>
     </div>
 
     <div className="row">
-      <div className="col-md-2"></div>
-      <div className="col-md-8">
+      <div className="col-sm-12">
         <texarea style={{width: "100%"}} rows="4" cols="50">
           <span>To  be or not to be, that is the ... gazornanplatt.
           To  be or not to be, that is the ... gazornanplatt.
@@ -121,27 +94,22 @@ const HomePage = () => {
           To  be or not to be, that is the ... gazornanplatt. </span>
         </texarea>
       </div>
-      <div className="col-md-2"></div>
     </div>
 
     <div className="row">
-      <div className="col-md-2"></div>
-      <div className="col-md-8">
+      <div className="col-sm-12">
         <h4 style={{width: "100%"}}>Favorite Thing</h4>
       </div>
-      <div className="col-md-2"></div>
     </div>
 
     <div className="row">
-      <div className="col-md-2"></div>
-      <div className="col-md-8">
+      <div className="col-sm-12">
         <input style={{width: "100%"}} value="Coding"/>
       </div>
-      <div className="col-md-2"></div>
     </div>
 
     <div className="row">
-      <div className="col-md-12">&nbsp;</div>
+      <div className="col-sm-12">&nbsp;</div>
     </div>
 
 
